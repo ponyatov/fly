8f23dfd4e2ba3b9cc878580ab3f2d319f58b803e
Dmitry Ponyatov <dponyatov@gmail.com>
2019-05-08 09:10:21 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- fly.py
+++ fly.py
@@ -1,19 +1,24 @@
 import os,sys ; print sys.argv
 
+################################################################## frame system
+
 class Frame:
     def __init__(self,V):
-        self.type = self.__class__.__name__.lower()
-        self.val  = V
-        self.slot = {}
-        self.nest = []
-    def __setitem__(self,key,obj):
-        self.slot[key] = obj ; return self
-    def __floordiv__(self,obj):
-        self.nest.append(obj)
+        self.type  = self.__class__.__name__.lower()
+        self.val   = V
+        self.slot  = {}
+        self.nest  = []
+        self.immed = False
+        
+    ## dump
+    
     def __repr__(self):
         return self.dump()
     def dump(self,depth=0,prefix=''):
         tree = self.pad(depth) + self.head(prefix)
+        if not depth: Frame.dumped = []
+        if self in Frame.dumped: return tree + ' _/'
+        else: Frame.dumped.append(self)
         for i in self.slot:
             tree += self.slot[i].dump(depth+1,prefix='%s = '%i)
         for j in self.nest:
@@ -22,25 +27,167 @@ class Frame:
     def pad(self,N):
         return '\n' + '\t' * N
     def head(self,prefix=''):
-        return '%s<%s:%s> @%x' % (prefix, self.type, self.val, id(self)) 
+        return '%s<%s:%s> @%x' % (prefix, self.type, self.val, id(self))
+
+    ## manipulations
+    
+    def __getitem__(self,key):
+        return self.slot[key]
+    def __setitem__(self,key,obj):
+        self.slot[key] = obj ; return self
+    def __floordiv__(self,obj):
+        self.nest.append(obj)
+    def __lshift__(self,obj):
+        self[obj.val] = obj ; return self
+        
+    ## stack ops
+    
+    def top(self):
+        return self.nest[-1]
+    def pop(self):
+        return self.nest.pop()
+    
+    ## execution & code generation
+    
+    def execute(self):
+        S // self
+        
+    
+##################################################################### primitive
         
 class String(Frame): pass
+
+class Symbol(Frame): pass
+
+##################################################################### container
         
 class Stack(Frame): pass
 
+class Dict(Frame):
+    def __lshift__(self,F):
+        if callable(F): return self << Cmd(F)
+        else: return Frame.__lshift__(self, F)
+
+######################################################################## active
+
+class Cmd(Frame):
+    def __init__(self,F,I=False):
+        Frame.__init__(self, F.__name__)
+        self.fn = F
+        self.immed = I
+    def execute(self):
+        self.fn()
+        
+############################################################### metaprogramming
+
+class Meta(Frame): pass
+
+class Module(Meta): pass
+
+######################################################### virtual FORTH machine
+
 S = Stack('DATA')
 
-class Voc(Frame): pass
+W = Dict('FORTH')
 
-W = Voc('VOC')
-
-# W['W'] = W
+W['W'] = W
 W['S'] = S
 
+############################################################### metaprogramming
+
+def META(): S // Meta(S.pop().val)
+W << META
+
+def MODULE(): S // Module(S.pop().val)
+W << MODULE
+
+################################################################# manipulations
+
+def ST(): W[S.pop().val] = S.pop()
+W['!'] = Cmd(ST)
+
+def pST():
+    C = S.pop() ; B = S.pop() ; A = S.pop() ; C[B.val] = A
+W['.!'] = Cmd(pST)
+
+def LSHIFT(): B = S.pop() ; S.top() << B
+W['<<'] = Cmd(LSHIFT)
+
+######################################################################### debug
+
+def BYE(): sys.exit(0)
+W << BYE
+
+def QQ(): print W ; BYE()
+W['??'] = Cmd(QQ,I=True)
+
+############################################################# PLY powered lexer
+
+import ply.lex as lex
+
+tokens = ['symbol','string']
+
+t_ignore = ' \t\r\n'
+
+states = (('str','exclusive'),)
+t_str_ignore = ''
+
+def t_string(t):
+    r'\''
+    t.lexer.lexstring=''
+    t.lexer.push_state('str')
+def t_str_string(t):
+    r'\''
+    t.lexer.pop_state()
+    return String(t.lexer.lexstring)
+def t_str_char(t):
+    r'.'
+    t.lexer.lexstring += t.value
+
+def t_comment(t):
+    r'[\#\\].*\n'
+    pass
+def t_symbol(t):
+    r'[`]|[^ \t\r\n]+'
+    return Symbol(t.value)
+
+def t_ANY_error(t): raise SyntaxError(t)
+
+lexer = lex.lex()
+
+################################################################### interpreter
+
+def QUOTE():
+    WORD()
+W['`'] = Cmd(QUOTE,I=True)
+
+def WORD():
+    token = lexer.token()
+    if not token: return False
+    S // token  ; return True
+    
+def FIND():
+    token = S.pop()
+    try: S // W[token.val]
+    except KeyError:
+        try: S // W[token.val.upper()]
+        except KeyError:
+            raise SyntaxError(token)
+        
+def EXECUTE():
+    S.pop().execute()
+        
 def INTERPRET():
-    print W
+    lexer.input(S.pop().val)
+    while True:
+        if not WORD(): break
+        if isinstance(S.top(), Symbol):
+            FIND()
+        EXECUTE()
+        
+################################################################### system init
 
 infiles = sys.argv[1:] ; print infiles
 if not infiles: infiles += ['fly.fly']
 for i in infiles:
-    S // String(i) ; INTERPRET()
+    S // String(open(i).read()) ; INTERPRET()
